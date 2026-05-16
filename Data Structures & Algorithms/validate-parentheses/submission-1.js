7f0dc5568e9a1eb363e16a50dcc8f741f9fae984
Daren <39359573+darenbt@users.noreply.github.com>
2026-05-16 14:19:13 -0500

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.js
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isValid(braces) {
+        var matches = { '(':')', '{':'}', '[':']' };
+        var stack = [];
+        var currentChar;
+
+        for (var i=0; i<braces.length; i++) {
+            currentChar = braces[i];
+
+            if (matches[currentChar]) { // opening braces
+            stack.push(currentChar);
+            } else { // closing braces
+            if (currentChar !== matches[stack.pop()]) {
+                return false;
+            }
+            }
+        }
+
+        return stack.length === 0; // any unclosed braces left?
+    }
+}
